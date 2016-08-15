9bcd1040a22bb1f7d528a134348e40525e74461d
Lindsey Stickline <lindseystickline@Lindseys-Air.att.net>
2016-08-15 07:39:29 -0500

    Everything works adjusting the HTML, but couldn't get JS file to work.

---
+++ quiz.js
@@ -0,0 +1,3 @@
+//js document wasn't working the way I wanted it to, but I was able to get something working in the
+//html file using <script> tags. 
+//now working on making the code work in the quiz.js file, plus making the interactive text boxes work.
