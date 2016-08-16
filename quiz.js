e7e079b04f1e60b084d7277c542d27337ac35eb8
Lindsey Stickline <lindseystickline@Lindseys-MacBook-Air.local>
2016-08-15 19:33:06 -0500

    Minor fixes to HTML, 95% complete

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
 //js document wasn't working the way I wanted it to, but I was able to get something working in the
 //html file using <script> tags. 
 //now working on making the code work in the quiz.js file, plus making the interactive text boxes work.
+
