4eb88dc0fd8561e178cd1492c6f254bce39e5420
Lindsey Stickline <lindseystickline@Lindseys-MacBook-Air.local>
2016-08-20 13:53:19 -0500

    Final: the buttons finally work

--- quizagain.js
+++ quizagain.js
@@ -1,10 +1,50 @@
-var submit = document.getElementById('submit');
-submit.addEventListener("click", retrieveInputs);
-
-
 var treeInput = {rows: "", character: ""}
 
 
+// Submit Button 
+var submit = document.getElementById('submit');
+submit.addEventListener("click", maybeBuildTree);
+
+
+// Submit via pushing Enter
+var rowsEnter = document.getElementById('rowinput');
+rowsEnter.addEventListener("keypress", pushEnter);
+
+var characterEnter = document.getElementById('characterinput');
+characterEnter.addEventListener("keypress", pushEnter);
+
+function pushEnter(e) {
+	if (e.keyCode == 13) {
+		maybeBuildTree();
+	}
+}
+
+
+// Clear Button
+var clear = document.getElementById('clear');
+clear.addEventListener("click", clearAll);
+
+function clearAll() {
+	document.getElementById('rowinput').value = "";
+	document.getElementById('characterinput').value = "";
+}
+
+
+// This tests whether the fields are entered; if yes, execute retrieveInputs
+function maybeBuildTree() {
+	if (!rowinput.value) {
+		alert("You need to put stuff in the fields.");
+	}
+	else if (!characterinput.value) {
+		alert("Seriously, put stuff in the fields.");
+	}
+	else {
+		retrieveInputs();
+	}
+}
+
+
+// This grabs the rows & character, then executes treeBuilder
 function retrieveInputs() {
 	treeInput.rows = document.getElementById('rowinput').value;
 	treeInput.character = document.getElementById('characterinput').value;
